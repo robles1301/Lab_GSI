be9355a9df9e2d1fc5b56d59db3de1230e011069
carloosnaranjo <carlos.naranjo1@alu.uclm.es>
2025-04-01 19:54:55 +0200

    Creación del primer acercamiento del modelo y pruebas del mismo.

--- README.md
+++ README.md
@@ -1,7 +1,23 @@
 # Detección de Neumonía en Rayos X con CNN
 
-Proyecto de clasificación de imágenes médicas usando redes neuronales convolucionales para identificar neumonía en radiografías de tórax. 
+Proyecto de clasificación de imágenes médicas usando redes neuronales convolucionales para identificar neumonía en radiografías de tórax.
 
 ## Descripción
 
 El objetivo de este proyecto es desarrollar un modelo de clasificación de imágenes médicas que permita identificar la presencia de neumonía en radiografías de tórax. Para ello, se ha utilizado un dataset de imágenes de rayos X de tórax de pacientes con neumonía y sin neumonía. Usaremos redes neuronales convolucionales (CNN) con tecnologías como TensorFlow y Keras para entrenar el modelo.
+
+## Entrenamiento del modelo
+
+Basta con ejecutar el archivo `train_model.py`:
+
+```
+python3 train_model.py
+```
+
+## Probar el modelo
+
+Al ejecutar el archivo `test_model.py` se probará el modelo con el dataset de test y generará un archivo `predictions.csv` que contendrá para cada imagen analizada la valoración del modelo, siendo 0 sano y 1 con neumonía.
+
+```
+python3 test_model.py
+```
---
+++ test_model.py
@@ -0,0 +1,42 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+
+img_width, img_height = 150, 150
+batch_size = 1  
+
+test_dir = 'data/test'
+
+test_datagen = ImageDataGenerator(rescale=1./255)
+
+test_generator = test_datagen.flow_from_directory(
+    test_dir,
+    target_size=(img_width, img_height),
+    batch_size=batch_size,
+    class_mode='binary',
+    shuffle=False 
+)
+
+model = load_model('modelo_neumonia.h5')
+
+loss, accuracy = model.evaluate(test_generator, steps=test_generator.samples // batch_size)
+print(f'Pérdida en test: {loss:.4f}')
+print(f'Exactitud en test: {accuracy:.4f}')
+
+predictions = model.predict(test_generator, steps=test_generator.samples // batch_size)
+
+print("Predicciones (0: NORMAL, 1: PNEUMONIA):")
+print(predictions)
+
+# Interpretacion de los resultados
+predicted_classes = np.where(predictions > 0.5, 1, 0) # Redondear a 0 o 1
+print("Clases predichas:")
+print(predicted_classes)
+# Guardar las predicciones en un archivo CSV
+predicted_classes = predicted_classes.flatten()
+filenames = test_generator.filenames
+results = pd.DataFrame({"Filename": filenames, "Predicted Class": predicted_classes})
+results.to_csv("predictions.csv", index=False)
+print("Predicciones guardadas en predictions.csv")
